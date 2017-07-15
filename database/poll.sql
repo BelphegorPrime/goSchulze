bb15afbbb70ade00a96e9e256e3fe3648204abc8
Marcel Rösler <roesler1@gmx.de>
2017-07-15 13:03:34 +0200

    fixed typo, added tables

--- database/poll.sql
+++ database/poll.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `poll` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `poll`;
 -- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
 --
--- Host: 172.28.0.23    Database: poll
+-- Host: 172.29.0.23    Database: poll
 -- ------------------------------------------------------
 -- Server version	5.7.17
 
@@ -16,6 +18,36 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `awnser`
+--
+
+DROP TABLE IF EXISTS `awnser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `awnser` (
+  `ID` int(11) NOT NULL,
+  `questionID` int(11) DEFAULT NULL,
+  `value` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `possible_awnser`
+--
+
+DROP TABLE IF EXISTS `possible_awnser`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `possible_awnser` (
+  `ID` int(11) NOT NULL,
+  `questionID` int(11) DEFAULT NULL,
+  `value` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `question`
 --
 
@@ -26,7 +58,7 @@ CREATE TABLE `question` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
   `question` varchar(255) NOT NULL,
   `awnser` varchar(255) NOT NULL,
-  `endtime` TIMESTAMP NOT NULL,
+  `endtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -40,4 +72,4 @@ CREATE TABLE `question` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-11 17:02:31
+-- Dump completed on 2017-07-15 11:54:55
